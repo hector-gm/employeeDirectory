eac6697bf1fe3bf5d9131944eabe46909d00ec20
Hector Garcia Millares <hgarcia.millares@gmail.com>
2021-02-25 22:05:13 -0500

    Updated components folders and built first instance of App.js

--- src/App.js
+++ src/App.js
@@ -1,2 +1,35 @@
-import React from "react";
+import React, { Component } from "react";
+import EmployeeCard from "./components/EmployeeCard";
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
+import employees from "./database/employees";
+
+class App extends Component {
+    state= {
+        employees
+    }
+
+
+    render() {
+        return (
+            <Wrapper>
+                <Title>Employee Directory</Title>
+                {this.state.employees.map(employee => (
+                    <EmployeeCard
+                    id={employee.id}
+                    key={employee.id}
+                    name={employee.name}
+                    picture={employee.picture}
+                    email={employee.email}
+                    phone={employee.phone}
+                    cell={employee.cell}
+                    timezone={employee.timezone}
+                    />
+                ))}
+            </Wrapper>
+        );
+    }
+}
+
+export default App;
 
