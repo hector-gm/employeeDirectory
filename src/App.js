56d9574e57229af9b6182bec7ac81733ffd21ba9
Hector Garcia Millares <hgarcia.millares@gmail.com>
2021-02-27 22:33:12 -0500

    Reworked app, new attempt to render

--- src/App.js
+++ src/App.js
@@ -1,18 +1,53 @@
 import React from "react";
-import Wrapper from "./components/Wrapper";
+import "./App.css";
 import Header from "./components/Header";
-import DirectoryContainer from "./components/DirectoryContainer";
+import TableHeader from "./components/TableHeader";
 
-function App() {
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      search: "",
+      peopleArray: [],
+      isAlphabetical: true,
+    }
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  handleClick() {
+    this.setState({ isAlphabetical: !this.state.isAlphabetical })
+  }
+  componentDidMount() {
+    fetch("https://randomuser.me/api/?results=25")
+      .then(response => response.json())
+      .then(response => {
+        for (let i = 0; i < response.results.length; i++) {
+          let curResult = response.results[i]
+          curResult.fullName = curResult.name.first + " " + curResult.name.last
+          curResult.fixedDate = curResult.dob.date
+        }
+        this.setState({ peopleArray: response.results })
+      })
+  }
+  render() {
+    let searchState = this.state.search
+
     return (
-    <div className="App"> 
-        <Wrapper>
-            <Header />
-                <DirectoryContainer />
-        </Wrapper>
-    </div>
-    );
+      <div>
+        <Header />
+        <input
+          value={searchState}
+          onChange={e => this.setState({ search: e.target.value })}
+        />
+        <TableHeader
+          search={searchState}
+          people={this.state.peopleArray}
+          isNameAscending={this.state.isAlphabetical}
+          handleClick={this.handleClick}
+        />
+      </div>
+    )
+  }
 }
 
 export default App;
-
