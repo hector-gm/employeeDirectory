2fd4f3355fd819ddb60153e7f66a8dde4c4f7232
Hector Garcia Millares <hgarcia.millares@gmail.com>
2021-02-27 20:03:37 -0500

    Revisiting components and general app structure to solve no content displayed

--- src/App.js
+++ src/App.js
@@ -1,34 +1,17 @@
 import React from "react";
-import EmployeeCard from "./components/EmployeeCard";
 import Wrapper from "./components/Wrapper";
-import Title from "./components/Title";
-import employees from "./employees";
+import Header from "./components/Header";
+import DirectoryContainer from "./components/DirectoryContainer";
 
-class App extends React.Component {
-    state = {
-        employees
-    }
-
-    render() {
-        return (
-            <Wrapper>
-                <Title>Employee Directory</Title>
-                {this.state.employees.map(employee => (
-                    <EmployeeCard
-                    id={employee.id}
-                    key={employee.id}
-                    name={employee.name}
-                    picture={employee.picture.medium}
-                    email={employee.email}
-                    phone={employee.phone}
-                    cell={employee.cell}
-                    timezone={employee.location.timezone}
-                    />
-                    ))
-                }
-            </Wrapper>
-        );
-    }
+function App() {
+    return (
+    <div className="App"> 
+        <Wrapper>
+            <Header />
+                <DirectoryContainer />
+        </Wrapper>
+    </div>
+    );
 }
 
 export default App;
---
+++ src/components/Title.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Title () {
+    return (
+        <div className="Title">
+            <h2>Employee Directory</h2>
+        </div>
+    );
+}
+
+export default Title;
